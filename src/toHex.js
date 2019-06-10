0a4aed01e563e244c80ed97fd851f07b8dbead7c
vpolets <vpolets@gmail.com>
2019-06-10 13:18:52 +0300

    add task and tests

---
+++ src/toHex.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * The function takes a number and returns a value - the record of this number in the hexadecimal number system
+ * .toString(16) cannot be used
+ *
+ * Divide the number by 16 with the remainder
+ * 50/16 = 3 (remainder 2)
+ * 3/16 = 0 (remainder 3)
+ * When the whole part is 0, then we take all the balances in the reverse order
+ * 50 === 0x32
+ *
+ *
+ * @param {number} value
+ *
+ * @return {string}
+ */
+function toHex(value) {
+  return '1a4';
+}
+
+module.exports = toHex;
---
+++ src/toHex.test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const toHex = require('./toHex');
+
+test(`Should return hexadecimal number string`, () => {
+  expect(toHex(42).toLowerCase())
+    .toBe('2a');
+});
+
+test(`Should return zero`, () => {
+  expect(toHex(0).toLowerCase())
+    .toBe('0');
+});
