33ae2aed741426dae5a8c86ffa13ec84fa9dae92
vpolets <vpolets@gmail.com>
2019-06-11 17:23:50 +0300

    fix task description v2

--- src/toHex.js
+++ src/toHex.js
@@ -4,14 +4,16 @@
  * The function takes a number and returns
  * its hexadecimal representation as a lower
  * case string. For example 256 is converted
- * то ff. You should not use .toString() method.
+ * то 'ff'. You should not use .toString() method.
  *
  * Divide the number by 16 with the remainder
  * 50/16 = 3 (remainder 2)
  * 3/16 = 0 (remainder 3)
  * When the whole part is 0, then we take all
  * the remainders in the reverse order
- * 50 === 32
+ *
+ * Conversion algorithm:
+ * 50 === '32'
  *
  *
  * @param {number} value
