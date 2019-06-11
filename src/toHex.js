72e209fb5ffb72afef5a9a69eb3da934f621d578
vpolets <vpolets@gmail.com>
2019-06-11 17:49:47 +0300

    fix task description v2.2

--- src/toHex.js
+++ src/toHex.js
@@ -6,13 +6,13 @@
  * case string. For example 256 is converted
  * то 'ff'. You should not use .toString() method.
  *
+ * Conversion algorithm:
  * Divide the number by 16 with the remainder
  * 50/16 = 3 (remainder 2)
  * 3/16 = 0 (remainder 3)
  * When the whole part is 0, then we take all
  * the remainders in the reverse order
  *
- * Conversion algorithm:
  * 50 === '32'
  *
  * @param {number} value
