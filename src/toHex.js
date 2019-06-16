ee28399009e5e5c7b17383424f76f6f6c16a5258
OleksandrHlybchenko <logista@meta.ua>
2019-06-16 14:14:37 +0300

    :bug: fix

--- src/toHex.js
+++ src/toHex.js
@@ -25,9 +25,9 @@ function toHex(value) {
   const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
 
   if (value < 16) {
-    return `${secondStep}`;
+    return `${hex[secondStep]}`;
   } else {
-    return `${secondStep}${hex[firstStep]}`;
+    return `${hex[secondStep]}${hex[firstStep]}`;
   }
 }
 
