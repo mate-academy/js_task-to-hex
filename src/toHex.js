4b490b03ea3ae82b65f77a712a3b7595773f3ef9
ivan_safiulin <ivansafiulin@myrambler.ru>
2019-08-14 20:36:51 +0300

    Solution

--- src/toHex.js
+++ src/toHex.js
@@ -19,8 +19,40 @@
  *
  * @return {string}
  */
+
 function toHex(value) {
-  // write code here
+  const hexList = {
+    0: '0',
+    1: '1',
+    2: '2',
+    3: '3',
+    4: '4',
+    5: '5',
+    6: '6',
+    7: '7',
+    8: '8',
+    9: '9',
+    10: 'a',
+    11: 'b',
+    12: 'c',
+    13: 'd',
+    14: 'e',
+    15: 'f',
+  };
+
+  let restValue = value;
+  const resultToHexList = [];
+
+  while (true) {
+    if (Math.floor(restValue / 16) > 0) {
+      resultToHexList.push(hexList[Math.floor(restValue % 16)]);
+      restValue = Math.floor(restValue / 16);
+    } else {
+      resultToHexList.push(hexList[Math.floor(restValue % 16)]);
+      break;
+    }
+  }
+  return resultToHexList.reverse().join('');
 }
 
 module.exports = toHex;
