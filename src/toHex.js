ad18da3bb88f51a5a243b1291cdec06b4384bb7e
Ihor Stavratii <igor.stavratiy@gmail.com>
2019-11-12 16:13:20 +0200

    refactoring

--- src/toHex.js
+++ src/toHex.js
@@ -22,24 +22,19 @@
 function toHex(value) {
   const hex = 16;
   const container = [];
-  const hexLetters = ['a', 'b', 'c', 'd', 'e', 'f'];
+  const hexLetters = '0123456789abcdef';
   let numberDecimal = value;
 
   if (numberDecimal !== 0) {
     while (Math.floor(numberDecimal) > 0) {
       const remainder = numberDecimal % hex;
-
-      if (remainder >= 10) {
-        container.push(hexLetters[remainder - 10]);
-      } else {
-        container.push(Math.floor(remainder));
-      }
-
+      container.push(hexLetters[Math.floor(remainder)]);
       numberDecimal /= hex;
     }
 
     return container.reverse().join('');
   }
+
   return '0';
 }
 
