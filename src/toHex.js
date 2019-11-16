aa28253450f428dfbd4417646940dbdd1f18a8a1
Oles Marola <oles_pw@ukr.net>
2019-11-16 19:41:07 +0200

    to hex

--- src/toHex.js
+++ src/toHex.js
@@ -8,19 +8,31 @@
  *
  * Conversion algorithm:
  * Divide the number by 16 with the remainder
- * 50/16 = 3 (remainder 2)
+ * 59/16 = 3 (remainder 11)
  * 3/16 = 0 (remainder 3)
  * When the whole part is 0, then we take all
  * the remainders in the reverse order
  *
- * 50 === '32'
+ * 50 === '3b'
  *
  * @param {number} value
  *
  * @return {string}
  */
 function toHex(value) {
-  // write code here
+  if (value === 0) {
+    return '0';
+  }
+
+  const hexAlphabet = '0123456789abcdef';
+  let hexValue = '';
+  let reverseValue = value;
+
+  while (reverseValue > 0) {
+    hexValue += hexAlphabet[reverseValue % 16];
+    reverseValue = Math.floor(reverseValue / 16);
+  }
+  return hexValue.split('').reverse().join('');
 }
 
 module.exports = toHex;
