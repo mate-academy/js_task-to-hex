6408b046383e52708bfdf27e8a9ddac467d48bb7
Viacheslav <slavic.lavrenyuk@gmail.com>
2020-01-21 20:02:32 +0200

    Convert to HEX Solution

--- src/toHex.js
+++ src/toHex.js
@@ -20,7 +20,42 @@
  * @return {string}
  */
 function toHex(value) {
-  // write code here
+  let hex = '';
+  let decimal = value;
+
+  if (decimal === 0) {
+    return '0';
+  }
+
+  while (decimal > 0) {
+    const REMAINDER = (decimal % 16);
+
+    switch (REMAINDER) {
+      case 10:
+        hex += 'a';
+        break;
+      case 11:
+        hex += 'b';
+        break;
+      case 12:
+        hex += 'c';
+        break;
+      case 13:
+        hex += 'd';
+        break;
+      case 14:
+        hex += 'e';
+        break;
+      case 15:
+        hex += 'f';
+        break;
+      default:
+        hex += (decimal % 16);
+    }
+    decimal = Math.trunc(decimal / 16);
+  }
+
+  return hex.split('').reverse().join('');
 }
 
 module.exports = toHex;
