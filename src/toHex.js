2f77fd0e69d7a96604e878484b4f10468a4025dd
Tata2222 <tataolcha@gmail.com>
2020-03-29 22:52:35 +0300

    solution

--- src/toHex.js
+++ src/toHex.js
@@ -20,7 +20,16 @@
  * @return {string}
  */
 function toHex(value) {
-  // write code here
+  const numeric = value;
+  const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
+  const shot = hex[Math.floor(numeric / 16)];
+  const remainder = hex[numeric % 16];
+
+  if (numeric === 0) {
+    return '0';
+  }
+
+  return '' + shot + remainder;
 }
 
 module.exports = toHex;
