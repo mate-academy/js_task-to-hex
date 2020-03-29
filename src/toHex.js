e6cd187ab00c8aabe71df501109741af77318a9d
Illia Oliynik <illiaoliynik@gmail.com>
2020-03-29 20:27:46 +0300

    solution

--- src/toHex.js
+++ src/toHex.js
@@ -17,10 +17,42 @@
  *
  * @param {number} value
  *
+ * 42 / 16 = 2
+ * 10 = a
+ * 2a
+ *
+ * 422
+ * 422 / 16 = 26 / 16 = 1 10
+ * 6
  * @return {string}
  */
 function toHex(value) {
-  // write code here
+  let Hex = '';
+  const systemTo16 = [0, 1, 2, 3, 4, 5, 6, 7, 8,
+    9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
+  const convertToLetter = function(element) {
+    if (element > 9) {
+      // eslint-disable-next-line no-param-reassign
+      element = systemTo16[element];
+    }
+
+    return element;
+  };
+  let intPart = 0;
+  let floatPart = 0;
+
+  if (value === 0) {
+    return '0';
+  }
+  intPart = (value - (value % 16)) / 16;
+  intPart = convertToLetter(intPart);
+  Hex += intPart + '';
+  floatPart = (value % 16);
+  floatPart = convertToLetter(floatPart);
+  Hex += floatPart + '';
+
+  return Hex;
 }
 
 module.exports = toHex;
