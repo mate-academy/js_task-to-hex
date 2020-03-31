b7d1f4397552726754640b1dfac82523ba5b6331
Eugene <evgnkuchma@gmail.com>
2020-03-31 05:43:28 +0300

    solution

--- src/toHex.js
+++ src/toHex.js
@@ -20,7 +20,15 @@
  * @return {string}
  */
 function toHex(value) {
-  // write code here
+  if (value === 0) {
+    return '0';
+  }
+
+  const toHexArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
+  const first = toHexArr[Math.floor(value / 16)];
+  const second = toHexArr[value % 16];
+
+  return `${first}${second}`;
 }
 
 module.exports = toHex;
