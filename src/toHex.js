36d3ac6e43422e5d1a6ed37ce090580eb081d38f
Liliia <chuiko1112@gmail.com>
2020-05-21 17:13:50 +0300

    Solution

--- src/toHex.js
+++ src/toHex.js
@@ -20,7 +20,17 @@
  * @return {string}
  */
 function toHex(value) {
-  // write code here
-}
+  const hexArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+  let rem = value;
+  let hex = '';
 
+  while (rem > 16) {
+    const devide = Math.floor(rem / 16);
+
+    rem = rem % 16;
+    hex += hexArr[devide];
+  }
+
+  return hex + hexArr[rem];
+}
 module.exports = toHex;
