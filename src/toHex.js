196dd00a378fa622e77254bbe7535e605c9cb08e
Vovasef <vovasef@gmail.com>
2020-05-22 01:01:48 +0300

    Solution

--- src/toHex.js
+++ src/toHex.js
@@ -20,7 +20,34 @@
  * @return {string}
  */
 function toHex(value) {
-  return value.toString(16);
+  let hex = '';
+  const hexTable = {
+    0: '0',
+    1: '1',
+    2: '2',
+    3: '3',
+    4: '4',
+    5: '5',
+    6: '6',
+    7: '7',
+    8: '8',
+    9: '9',
+    10: 'a',
+    11: 'b',
+    12: 'c',
+    13: 'd',
+    14: 'e',
+    15: 'f',
+  };
+  const remainder = value % 16;
+
+  const newvalue = Math.floor(value / 16);
+
+  if (newvalue > 0) {
+    hex = toHex(newvalue);
+  }
+
+  return hex + hexTable[remainder];
 }
 
 module.exports = toHex;
