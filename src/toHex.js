c685953814adbe165656c33948345fbf55bee4d9
VlaDinter <vladyslavdinter@gmail.com>
2020-05-22 05:02:46 +0300

    Solution

--- src/toHex.js
+++ src/toHex.js
@@ -1,26 +1,39 @@
 'use strict';
 
-/**
- * The function takes a number and returns
- * its hexadecimal representation as a lower
- * case string. For example 255 is converted
- * то 'ff'. You should not use .toString() method.
- *
- * Conversion algorithm:
- * Divide the number by 16 with the remainder
- * 50/16 = 3 (remainder 2)
- * 3/16 = 0 (remainder 3)
- * When the whole part is 0, then we take all
- * the remainders in the reverse order
- *
- * 50 === '32'
- *
- * @param {number} value
- *
- * @return {string}
- */
 function toHex(value) {
-  // write code here
+  if (!value) {
+    return '0';
+  }
+
+  function toHexNumber(number) {
+    switch (number) {
+      case 10:
+        return 'a';
+
+      case 11:
+        return 'b';
+
+      case 12:
+        return 'c';
+
+      case 13:
+        return 'd';
+
+      case 14:
+        return 'e';
+
+      case 15:
+        return 'f';
+
+      default:
+        return number + '';
+    }
+  }
+
+  const firstHexNumber = toHexNumber(Math.floor(value / 16));
+  const secondtHexNumber = toHexNumber(value % 16);
+
+  return firstHexNumber + secondtHexNumber;
 }
 
 module.exports = toHex;
