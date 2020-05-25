406da7e28d4169ff737a32edbc6e695f89ba51d0
Tatiana Ivanchenko <tanyarz1996@gmail.com>
2020-05-25 14:44:30 +0300

    solution

--- src/toHex.js
+++ src/toHex.js
@@ -20,15 +20,18 @@
  * @return {string}
  */
 function toHex(value) {
-  if (value < 0) {
-    return '0';
+  const hexSymbol = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    'a', 'b', 'c', 'd', 'e', 'f'];
+  const hexNumber = [];
+
+  if (value !== 0) {
+    hexNumber.push(hexSymbol[Math.floor(value / 16)]);
+    hexNumber.push(hexSymbol[value % 16]);
+
+    return hexNumber.join('');
   }
 
-  if (value > 255) {
-    return 'ff';
-  }
-
-  return (Number(value).toString(16)).slice(-2);
+  return '0';
 }
 
 module.exports = toHex;
