322467d49d372f261ff65e07abc33a0c6491c6fc
German <germandolia@gmail.com>
2020-05-25 13:32:26 +0300

    fix

--- src/toHex.js
+++ src/toHex.js
@@ -20,24 +20,7 @@
  * @return {string}
  */
 function toHex(value) {
-  const hexTable = {
-    0: '0',
-    1: '1',
-    2: '2',
-    3: '3',
-    4: '4',
-    5: '5',
-    6: '6',
-    7: '7',
-    8: '8',
-    9: '9',
-    10: 'a',
-    11: 'b',
-    12: 'c',
-    13: 'd',
-    14: 'e',
-    15: 'f',
-  };
+  const hexTable = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 
   let decValue = value;
   let hexString = '';
